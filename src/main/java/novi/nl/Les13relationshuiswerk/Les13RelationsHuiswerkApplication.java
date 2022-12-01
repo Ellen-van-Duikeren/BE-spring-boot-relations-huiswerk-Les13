a0c1a08ef9e9b8480291338134aebfb84a416a1b
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-12-01 19:43:24 +0100

    First commit

---
+++ src/main/java/novi/nl/Les13relationshuiswerk/Les13RelationsHuiswerkApplication.java
@@ -0,0 +1,13 @@
+package novi.nl.Les13relationshuiswerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les13RelationsHuiswerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les13RelationsHuiswerkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novi/nl/Les13relationshuiswerk/Les13RelationsHuiswerkApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.nl.Les13relationshuiswerk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les13RelationsHuiswerkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
